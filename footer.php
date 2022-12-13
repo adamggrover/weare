2fc7be5f5d862de50dbac04274cc4b6e2940130b
Adam Grover <adamggrover@gmail.com>
2022-12-13 15:04:56 +0000

    Replace with Weare info

--- footer.php
+++ footer.php
@@ -10,17 +10,15 @@
             </div>
             <div class="row footer-text">
                 <div class="col-md-4 footer-contact footer-column">
-                    <p class="mb-3">The Kings of Wessex Academy<br>Station Road, Cheddar<br>Somerset BS27 3AQ</p>
-                    <p class="mb-4">Headteacher: Mr David Wiltshire</p>
-                    <p><i class="fa-solid fa-phone"></i><span class="footer-spacer"></span>01934 742 608</p>
-                    <p><a href="mailto:office@kowessex.co.uk"><i class="fa-solid fa-envelope"></i></i><span class="footer-spacer"></span>Email Us</a></p>
-                    <p class="mb-4"><a href="https://www.google.com/maps/dir//The+Kings+of+Wessex+Academy+Station+Rd+Cheddar+BS27+3AQ/@51.2744878,-2.7786577,15z/data=!4m8!4m7!1m0!1m5!1m1!1s0x48721dd466cc4fd1:0x35a655c7fba40d1!2m2!1d-2.7786577!2d51.2744878" target="_blank" ><i class="fa-solid fa-location-dot"></i></i><span class="footer-spacer"></span>Get Directions</a></p>
+                    <p class="mb-3">Weare Academy<br>Notting Hill Way, Weare<br>Somerset BS26 2JS</p>
+                    <p class="mb-4">Executive Headteacher: Emma Tovey</p>
+                    <p><i class="fa-solid fa-phone"></i><span class="footer-spacer"></span>01934 732270</p>
+                    <p><a href="mailto:rainbow-woods@weareacademy.co.uk"><i class="fa-solid fa-envelope"></i></i><span class="footer-spacer"></span>Email Us</a></p>
+                    <p class="mb-4"><a href="https://www.google.com/maps/dir//Weare+Academy+First+School+and+Rainbow+Woods+Pre-School,+Notting+Hill+Way,+Weare,+Axbridge/@51.2733777,-2.8855986,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x48721d21c9005ee1:0x223018a0fa07df35!2m2!1d-2.8505693!2d51.2733289" target="_blank" ><i class="fa-solid fa-location-dot"></i></i><span class="footer-spacer"></span>Get Directions</a></p>
 
-                    <div class="footer-social mb-4"><a href="https://twitter.com/kowessex" target="_blank" ><i class="fa-brands fa-twitter fa-xl"></i></a><span class="footer-spacer"></span><span class="footer-spacer"></span><a href="https://www.facebook.com/kowessex/" target="_blank"><i class="fa-brands fa-facebook-f fa-xl"></i></a></div>
+                    <div class="footer-social mb-4"><a href="https://twitter.com/weare_school" target="_blank" ><i class="fa-brands fa-twitter fa-xl"></i></a><span class="footer-spacer"></span><span class="footer-spacer"></span><a href="https://en-gb.facebook.com/weareacademyschool/" target="_blank"><i class="fa-brands fa-facebook-f fa-xl"></i></a></div>
 
-                    <p class="mt-4">A charitable company limited by guarantee<br>
-                        Registered in England<br>
-                        Company Number 7348580</p>
+                    <p class="mt-4">
                     
                 </div>
                 <div class="col-md-4 footer-links footer-column">
