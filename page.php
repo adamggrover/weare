c3f79767ea2dbfd8ff15c393ae22fd243fb7132d
Adam Grover <adamggrover@gmail.com>
2022-12-15 12:31:11 +0000

    Change email address link to Weare

--- page.php
+++ page.php
@@ -24,7 +24,7 @@
                     <div class="content-area-social-links">
                         <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" target="_blank" data-show-count="false"><i class="fa-brands fa-twitter ps-2 fa-xl"></i></a>
                         <a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank"><i class="fa-brands fa-facebook-f ps-2 fa-xl"></i></a>
-                        <a href="mailto:office@kowessex.co.uk"><i class="fa-solid fa-envelope ps-2 fa-xl"></i></a>   
+                        <a href="mailto:office@weareacademy.co.uk"><i class="fa-solid fa-envelope ps-2 fa-xl"></i></a>   
 
                     </div>
                     
