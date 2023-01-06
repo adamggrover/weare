1f8a5accf0e2e7c0d97cd1e9f44b46de1041790b
Adam Grover <adamggrover@gmail.com>
2023-01-06 13:57:08 +0000

    Update site title

--- header.php
+++ header.php
@@ -4,18 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>The Kings of Wessex Academy, Cheddar</title>
+    <title>Weare Academy First School and Rainbow Woods Pre-School</title>
     <link rel="icon" type="image/x-icon" href="Assets/logos/kings-icon.svg">
 
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-58589832-1" defer></script>
-    <script defer>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-58589832-1');
-    </script>
 
 
 
